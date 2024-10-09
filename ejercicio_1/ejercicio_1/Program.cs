01b327cd7032d235fa15ae9dfb71cd122f088276
Juan Gonzalez <juanrafa1592@hotmail.com>
2024-10-09 14:02:45 -0500

    declarar variables y luego condicionales

--- ejercicio_1/ejercicio_1/Program.cs
+++ ejercicio_1/ejercicio_1/Program.cs
@@ -1,7 +1,17 @@
 ﻿public class Program
 {
+    // Desarrollar el juego “Adivina el número” en un programa de C#. El juego es muy sencillo, consiste en adivinar un número ℕ aleatorio que el sistema escoge de un determinado rango y lo guarda en memoria sin ser revelado.
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Adivina el numero");
+        Console.WriteLine("Ingrese el número de jugadores (2-4): ");
+        int.Parse(Console.ReadLine());
+        int numerodejugadores;
+
+
+
+
+
     }
 }
